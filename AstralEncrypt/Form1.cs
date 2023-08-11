db211e1e184096165470cbcbb46c0c9190d53292
amn <102465106+Amine945222@users.noreply.github.com>
2023-08-11 12:45:02 +0200

    Date : 11/08/2023 Heure : 12:44

---
+++ AstralEncrypt/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AstralEncrypt
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        public Form1()
+        {
+            InitializeComponent();
+            // Ajouter les événements MouseDown et MouseMove pour déplacer le formulaire avec la souris
+            MouseDown += Form1_MouseDown;
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Capture = false;
+                Message msg = Message.Create(this.Handle, WM_NCLBUTTONDOWN, (IntPtr)HT_CAPTION, (IntPtr)0);
+                this.WndProc(ref msg);
+            }
+        }
+    }
+}
